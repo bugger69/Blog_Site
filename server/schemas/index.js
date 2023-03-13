48e3197cc3b6a2a3e3a5bc9356368666b0bc6de7
bugger69 <aatreya21@gmail.com>
2023-03-13 18:24:21 +0530

    added html sanitization

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,16 +1,39 @@
-const Joi = require("joi"); // TODO : ADD HTML-SANITIZE TO THIS
+const baseJoi = require("joi"); // TODO : ADD HTML-SANITIZE TO THIS
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+  type: 'string',
+  base: joi.string(),
+  messages: {
+      'string.escapeHTML' : '{{#label}} must not include HTML!'
+  },
+  rules: {
+      escapeHTML: {
+          validate(value, helpers) {
+              const clean = sanitizeHtml(value, {
+                  allowedTags: [],
+                  allowedAttributes: {}
+              });
+              if(clean !== value) return helpers.error('string.escapeHTML', { value })
+              return clean;
+          }
+      }
+  }
+});
+
+const Joi = baseJoi.extend(extension);
 
 module.exports.blogSchema = Joi.object({
-  title: Joi.string().required(),
-  titleImage: Joi.string(),
-  content: Joi.string().required(),
+  title: Joi.string().required().escapeHTML(),
+  titleImage: Joi.string().escapeHTML(),
+  content: Joi.string().required().escapeHTML(),
   likes: Joi.number(),
-  author: Joi.string(),
+  author: Joi.string().escapeHTML(),
   comments: Joi.array(),
-  token: Joi.string().required(),
+  token: Joi.string().required().escapeHTML(),
 });
 
 module.exports.commentSchema = Joi.object({
-  content: Joi.string().required(),
-  token: Joi.string().required(),
+  content: Joi.string().required().escapeHTML(),
+  token: Joi.string().required().escapeHTML(),
 });
